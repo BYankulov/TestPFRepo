4a578f09c0b40a59ac74469208f5c2da4c25b6e6
byankulov <BYankulov@users.noreply.github.com>
2015-06-29 16:34:54 +0300

    Update of class path with maven depends.

--- hellomaven/src/main/java/com/example/hellomaven/HelloMavenMain.java
+++ hellomaven/src/main/java/com/example/hellomaven/HelloMavenMain.java
@@ -7,8 +7,8 @@ public class HelloMavenMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Logger logger = LoggerFactory.getLogger(HelloMavenMain.class);
-		//logger.info("Hell-o-o Maven!");
+		Logger logger = LoggerFactory.getLogger(HelloMavenMain.class);
+		logger.info("Hell-o-o Maven!");
 		System.out.println("Test line.");
 	}
 
